4b6c505a84afdf3618c233b369314b329f5c3138
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2020-07-19 12:40:21 -0500

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,31 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🐼",
+    name: "Panda Emoji",
+    meaning:
+      "A friendly, cartoon-styled face of a panda—a bear-like mammal known for eating bamboo—looking straight ahead. "
+  },
+  {
+    id: 4,
+    emoji: "😴",
+    name: "Sleeping Face",
+    meaning:
+      "A yellow face with eyes closed and mouth letting out three, cartoon-styled Zzz’s overhead to indicate it's sound asleep. The Zzz’s appear blue or purple across most platforms. "
+  }
+];
+export default emojipedia;
